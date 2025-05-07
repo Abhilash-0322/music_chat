ac86a929a199354cbdb135055ec3e661a308c90c
Abhilash-0322 <mauryaabhi2003@gmail.com>
2025-05-08 04:05:51 +0530

    adminpage

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,6 @@
 import { clerkClient } from "@clerk/express";
 
+
 export const protectedRoute = async(req,res,next) => {
     if(!req.auth.userId){
         return res.status(401).json({message: 'Unauthorized'});
@@ -10,6 +11,7 @@ export const protectedRoute = async(req,res,next) => {
 export const requireAdmin=async(req,res,next)=>{
     try{
         const currntUser = await clerkClient.getUser(req.auth.userId);
+        console.log(currntUser.PrimaryEmailAddress?.emailAddress);
         const isAdmin = process.env.ADMIN_EMAIL===currntUser.PrimaryEmailAddress?.emailAddress;
         if(!isAdmin){
             return res.status(403).json({message: 'Unauthorized- You must be an Admin'});
--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -2,6 +2,8 @@ import { useAuthStore } from '@/stores/useAuthStore'
 import React from 'react'
 import Header from './components/Header';
 import DashboardStats from './components/DashboardStats';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Album, Music } from 'lucide-react';
 
 const AdminPage = () => {
 
@@ -18,6 +20,18 @@ const AdminPage = () => {
 
     <Header/>
     <DashboardStats/>
+    <Tabs defaultValue='songs' className='space-y-6'>
+        <TabsList className='p-1 bg-zinc-800/50'>
+            <TabsTrigger value={'songs'} className='data-[state=active]:bg-zinc-800'>
+                <Music className='mr-2 size-4 '/>
+                Songs
+            </TabsTrigger>
+            <TabsTrigger value={'albums'} className='data-[state=active]:bg-zinc-800'>
+                <Album className='mr-2 size-4 '/>
+                Albums
+            </TabsTrigger>
+        </TabsList>
+    </Tabs>
     </div>
   )
 }
