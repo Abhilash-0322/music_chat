7de645145fa42983a745c32d9d2134b21c1a6c01
Abhilash-0322 <mauryaabhi2003@gmail.com>
2025-08-09 13:27:19 +0530

    sidebarfix

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -35,7 +35,7 @@
 
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Outlet } from "react-router-dom";
-// import LeftSidebar from "./components/LeftSidebar";
+import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
 import { PlaybackControls } from "./components/PlaybackControls";
@@ -60,7 +60,7 @@ const MainLayout = () => {
 				<AudioPlayer />
 				{/* left sidebar */}
 				<ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
-					{/* <LeftSidebar /> */}
+					<LeftSidebar />
 				</ResizablePanel>
 
 				<ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
